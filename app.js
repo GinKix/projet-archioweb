d9af90a98dcab8ce0593eb7a792df013c11c206c
Kilian Crettaz <kilian.crettaz@heig-vd.ch>
2021-10-10 12:16:52 +0200

    Provide DATABASE_URL to Heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/project-archioweb');
+mongoose.connect(process.env.DATABASE_URL || 'mongodb+srv://admin:<archioweb>@projet-archioweb.8sakj.mongodb.net/projet-archioweb?retryWrites=true&w=majority');
 
 var app = express();
 
