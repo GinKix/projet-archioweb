c0a1525a219658d09141f69998adb034c57f35c8
Jessica Luyet <jluyet@gmail.com>
2021-11-15 11:09:35 +0100

    Correction fermeture routes

--- routes/bar.js
+++ routes/bar.js
@@ -64,119 +64,118 @@ router.get("/api/bar", function (req, res, next) {
 
 
     });
+  });
+});
 
 
-    router.get("/api/bar/:IdBar", function (req, res, next) {
-      res.send("Afficher un bar "); // envoi de réponse au client
+router.get("/api/bar/:IdBar", function (req, res, next) {
+  res.send("Afficher un bar "); // envoi de réponse au client
 
 
-      // Bar.find()
+  // Bar.find()
+});
+
+
+router.put("/api/bar/:IdBar", function (req, res, next) {
+  //res.send("Modifier un bar "); // envoi de réponse au client
+
+  //futur contenu du middleware B
+  Bar.findById(req.params.IdBar, function (err, bar) {
+    if (err) {
+      return next(err);
+    } else if (!bar) {
+      return barNotFound(res, req.params.IdBar);
+    }
+
+    bar.update(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      debug(`Bar  updated: "${req.bar.name}"`);
+      res.sendStatus(200);
     });
+  });
+
+});
 
 
-    router.put("/api/bar/:IdBar", function (req, res, next) {
-      //res.send("Modifier un bar "); // envoi de réponse au client
+router.delete("/api/bar/:IdBar", function (req, res, next) {
+  //res.send("Supprimer un bar "); // envoi de réponse au client
+  Bar.findById(req.params.IdBar, function (err, bar) {
+    if (err) {
+      return next(err);
+    } else if (!bar) {
+      return barNotFound(res, req.params.IdBar);
+    }
 
-      //futur contenu du middleware B
-      Bar.findById(req.params.IdBar, function (err, bar) {
-        if (err) {
-          return next(err);
-        } else if (!bar) {
-          return barNotFound(res, req.params.IdBar);
-        }
 
-        bar.update(function (err) {
-          if (err) {
-            return next(err);
-          }
+    bar.remove(function (err) {
+      if (err) {
+        return next(err);
+      }
 
-          debug(`Bar  updated: "${req.bar.name}"`);
-          res.sendStatus(200);
-        });
-      });
-
+      debug(`Deleted bar "${req.bar.name}"`);
+      res.sendStatus(204);
     });
+  });
+
+});
 
 
-    router.delete("/api/bar/:IdBar", function (req, res, next) {
-      //res.send("Supprimer un bar "); // envoi de réponse au client
-      Bar.findById(req.params.IdBar, function (err, bar) {
-        if (err) {
-          return next(err);
-        } else if (!bar) {
-          return barNotFound(res, req.params.IdBar);
-        }
+//RATINGS
+router.get("/api/bar/:IdBar/rating", function (req, res, next) {
+  //res.send("Afficher le rating dun bar");
+
+  // envoi de réponse au client
+  /* A reprendre, probablement erreur, doit-on mettre savedScore ou Score?
+  
+  new Score(req.body).save(function (err, savedScore) {
+ 
+     Score.findById(req.params.IdScore.value, function (err, bar) {
+       if (err) {
+         return next(err);
+       } else if (!score) {
+         return barNotFound(res, req.params.IdScore);
+       }
+       debug(req.Score);
+       res
+       .send(savedScore)
+       .status(201);
+     });
+}); */
+});
+
+router.post("/api/bar/:IdBar/rating", function (req, res, next) {
+  //res.send("Ajouter une note à un bar"); // envoi de réponse au client
 
 
-        bar.remove(function (err) {
-          if (err) {
-            return next(err);
-          }
 
-          debug(`Deleted bar "${req.bar.name}"`);
-          res.sendStatus(204);
-        });
-      });
+});
 
+router.delete("/api/:IdBar/rating/:IdRating", function (req, res, next) {
+  //res.send("Supprimer une note à un bar"); // envoi de réponse au client
+
+  Score.findById(req.params.IdScore, function (err, savedScore) {
+    if (err) {
+      return next(err);
+    } else if (!person) {
+      return personNotFound(res, req.params.IdPerson);
+    }
+
+
+    person.remove(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      debug(`Deleted person "${req.person.username}"`);
+      res.sendStatus(204);
     });
+  });
 
+});
 
-    //RATINGS
-    router.get("/api/bar/:IdBar/rating", function (req, res, next) {
-      //res.send("Afficher le rating dun bar");
-
-      // envoi de réponse au client
-      /* A reprendre, probablement erreur, doit-on mettre savedScore ou Score?
-      
-      new Score(req.body).save(function (err, savedScore) {
-     
-         Score.findById(req.params.IdScore.value, function (err, bar) {
-           if (err) {
-             return next(err);
-           } else if (!score) {
-             return barNotFound(res, req.params.IdScore);
-           }
-           debug(req.Score);
-           res
-           .send(savedScore)
-           .status(201);
-         });
-    }); */
-    });
-
-    router.post("/api/bar/:IdBar/rating", function (req, res, next) {
-      //res.send("Ajouter une note à un bar"); // envoi de réponse au client
-
-
-
-    });
-
-    router.delete("/api/:IdBar/rating/:IdRating", function (req, res, next) {
-      //res.send("Supprimer une note à un bar"); // envoi de réponse au client
-
-      Score.findById(req.params.IdScore, function (err, savedScore) {
-        if (err) {
-          return next(err);
-        } else if (!person) {
-          return personNotFound(res, req.params.IdPerson);
-        }
-
-
-        person.remove(function (err) {
-          if (err) {
-            return next(err);
-          }
-
-          debug(`Deleted person "${req.person.username}"`);
-          res.sendStatus(204);
-        });
-      });
-
-    });
-
-    router.put("/api/bar/:IdBar/rating/:IdRating", function (req, res, next) {
-      res.send("Modifier la note du bar"); // envoi de réponse au client
-    });
-
-
-
+router.put("/api/bar/:IdBar/rating/:IdRating", function (req, res, next) {
+  res.send("Modifier la note du bar"); // envoi de réponse au client
+});
