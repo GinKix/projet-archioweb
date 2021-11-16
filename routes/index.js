e021430e9584f1458fe846f54f582e20d3c589c1
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2021-11-16 21:47:54 +0100

    les lignes superflues effacées : index.js

--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,6 @@ module.exports = router;
 //router.get = ce qu'il se passe quand on fait un get avec l'url de la route
 router.get('/nomRoute', function(req,res,next){
 
-  if ( navigator.geolocation ) {
-    // On demande d'envoyer la position courante 
-    navigator.geolocation.getCurrentPosition(function(position){
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      geolocation.user=(lat,lng);
-    }
-    )
-  }
-   
-    else {
-      // Function alternative sinon
-      alternative();
-    }
 
 res.send('bonjour'); // envoi de réponse au client
 });
