ecaf30785a833fc665ff54ce9532822f9719337c
Boumasmoud Yousra <yousra.boumasmoud@gmail.com>
2021-11-16 13:20:07 +0100

    Ajout de la position courante du user(navigateur)

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const user = require('../models/user');
 var router = express.Router();
 
 /* GET home page. */
@@ -12,5 +13,21 @@ module.exports = router;
 //TEST JESS 
 //router.get = ce qu'il se passe quand on fait un get avec l'url de la route
 router.get('/nomRoute', function(req,res,next){
+
+  if ( navigator.geolocation ) {
+    // On demande d'envoyer la position courante 
+    navigator.geolocation.getCurrentPosition(function(position){
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      geolocation.user=(lat,lng);
+    }
+    )
+  }
+   
+    else {
+      // Function alternative sinon
+      alternative();
+    }
+
 res.send('bonjour'); // envoi de réponse au client
 });
